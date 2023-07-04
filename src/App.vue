f33a6d87e85bc839e4f6725a0feca8f2bc745545
shiva <shiva@jaguarsoftwareindia.com>
2023-07-04 19:10:59 +0530

    added row and column

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,23 @@
 
   <main class="container mx-auto" style="height: 39rem">
     <div class="flex justify-center ...">
-      <h1 class="text-3xl py-4">Project with Vue3 , Tailwind and Pinia</h1>
+      <div class="grid gap-4 grid-flow-row auto-rows-max">
+        <div>
+          <h1 class="text-3xl py-4 text-green-900">
+            Project with Vue3 , Tailwind and Pinia
+          </h1>
+        </div>
+        <div v-for="task in taskStore.tasks" :key="task.id">
+          <div class="grid gap-6 grid-flow-col auto-cols-4">
+            <div>{{ task.id }}</div>
+            <div>{{ task.title }}</div>
+            <div>{{ task.isFav }}</div>
+            <div>
+              <button class="bg-red-500 text-white">delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 
@@ -18,12 +34,6 @@
       <p>&copy; 2023 My Website. All rights reserved.</p>
     </div>
   </footer>
-
-  <!-- {{ taskStore.name }}
-
-  <div v-for="task in taskStore.tasks" :key="task.id">
-    <p>{{ task.title }}</p>
-  </div> -->
 </template>
 
 <script>
